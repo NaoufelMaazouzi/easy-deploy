321b7fdf39ed1057a8bd3e4cc8fd5df2e7918a24
Naoufel <naoufel_du_28@live.fr>
2024-06-05 00:05:27 +0200

    fix bug

--- components/overview-sites-cta.tsx
+++ components/overview-sites-cta.tsx
@@ -23,8 +23,6 @@ export default async function OverviewSitesCTA() {
       View All Sites
     </Link>
   ) : (
-    <CreateSiteButton>
-      <CreateSiteModal />
-    </CreateSiteButton>
+    <CreateSiteButton />
   );
 }
